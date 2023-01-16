03117f5e4fd911b9e11bc1ce05d7965c7b31de71
Adson Henrique <adsonhenriquesilva@gmail.com>
2023-01-16 21:03:41 +0100

    test loadder

--- frontend/components/image.js
+++ frontend/components/image.js
@@ -4,13 +4,13 @@ import NextImage from "next/image"
 const Image = ({ image, style }) => {
   const { url, alternativeText, width, height } = image.data.attributes
 
-  // const loader = () => {
-  //   return getStrapiMedia(image)
-  // }
+  const loader = () => {
+    return getStrapiMedia(image)
+  }
 
   return (
     <NextImage
-      // loader={loader}
+      loader={loader}
       layout="responsive"
       width={width || "100%"}
       height={height || "100%"}
