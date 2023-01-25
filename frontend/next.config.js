6d63452cde15ad9495357eb92adfb5d70cd65541
Adson Henrique <adsonhenriquesilva@gmail.com>
2023-01-25 15:26:32 +0100

    Add amazon s3 url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
   images: {
     loader: "default",
     domains: [
-        "localhost",
-        "adsonatural-blog-backend-c4p7l.ondigitalocean.app"
+      "localhost",
+      "adsonatural-blog-backend-c4p7l.ondigitalocean.app",
+      "adsonatural-blog-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com",
     ],
   },
 }
