1e222cc215ef88151fd38bb94dbd83eac203369f
Adson Henrique <adsonhenriquesilva@gmail.com>
2023-01-29 00:45:53 +0100

    Add new entry to domains list

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
       "localhost",
       "adsonatural-blog-backend-c4p7l.ondigitalocean.app",
       "adsonatural-blog-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com",
+      "127.0.0.1",
     ],
   },
 }
